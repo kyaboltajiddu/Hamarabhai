844adbd5726e8ecad02f9a81cb47f6c8062765e0
kyaboltajiddu <121448257+kyaboltajiddu@users.noreply.github.com>
2022-12-26 21:37:27 +0530

    Add files via upload

---
+++ Q15Linearprobing.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+struct pair{
+    int value;
+    int key;
+};
+void display(struct pair ar[],int size);
+int main()
+{
+    int size,i,temp;
+    printf("Enter the size of the table : ");
+    scanf("%d",&size);
+    struct pair hash_table[size];
+    printf("Enter the elements : ");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&temp);
+        hash_table[temp%size].value=temp;
+        hash_table[temp%size].key=temp%size;
+    }
+    printf("\n");
+    display(hash_table,size);
+    return 0;
+}
+void display(struct pair ar[],int size)
+{
+    int i;
+     printf("Value\tKey\n");
+    for(i=0;i<size;i++)
+    {
+      printf("%d\t\t%d\n",ar[i].value,ar[i].key); 
+    }
+}
